7a1a48442e91c1e867b89a423f99fbcd71408183
Arun <arunid.surya@gmail.com>
2024-02-19 23:57:17 +0530

    Server initialized

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, console.log("Server is running on prt 5000"));
+app.get("/", (req, res) => {
+  res.send("Api is Running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((n) => n._id === id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
